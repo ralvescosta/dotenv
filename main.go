6d55beae5bd92053f251cd24fe2587aa343dba0c
Rafael Costa <rafael.rac.mg@gmail.com>
2021-06-10 13:33:25 -0300

    refactor: remove read func

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 	"strings"
 )
 
-const commentComa = "#"
+const commentCaractere = "#"
 
 func main() {}
 
 func Configure(path string) error {
-	file, err := readFile(path)
+	file, err := os.Open(path)
 	if err != nil {
 		return errors.New("error while opening env file")
 	}
@@ -26,14 +26,14 @@ func Configure(path string) error {
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		index := strings.Index(line, commentComa)
+		index := strings.Index(line, commentCaractere)
 		if index != -1 {
-			lineParsed := strings.Split(line, "#")
-			if len(lineParsed[0]) < 1 {
+			parsed := strings.Split(line, "#")
+			if len(parsed[0]) < 1 {
 				continue
 			}
 
-			v := strings.Split(lineParsed[0], "=")
+			v := strings.Split(parsed[0], "=")
 			if len(v[1]) > 1 {
 				os.Setenv(v[0], v[1])
 			}
@@ -51,13 +51,4 @@ func Configure(path string) error {
 	}
 
 	return nil
-}
-
-func readFile(path string) (*os.File, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	return file, nil
 }
